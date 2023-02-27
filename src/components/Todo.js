48aac6c5b5231e0b8bb890e5eeab3216a56dff28
FahimHassanMollah <fahimhassan0197@gmail.com>
2023-02-27 18:25:35 +0600

    updated

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,103 +1,178 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux'
-import { addToAllTdodos, addToCompletedTodos, addToInProgressTodos, addToNewTodos } from '../features/todos/todosSlice';
+import { addToAllTodos, addToCompletedTodos, addToInProgressTodos, addToNewTodos } from '../features/todos/todosSlice';
 import { DragDropContext, Droppable } from '@hello-pangea/dnd';
 import { Draggable } from '@hello-pangea/dnd';
 
 const Todo = () => {
-
     const [todo, setTodo] = useState("");
     const dispatch = useDispatch();
     const newTodos = useSelector(state => state.todos.newTodos);
     const inProgressTodos = useSelector(state => state.todos.inProgressTodos);
     const completedTodos = useSelector(state => state.todos.completedTodos);
-    const allTdodos = useSelector(state => state.todos.allTdodos);
+    const allTodos = useSelector(state => state.todos.allTodos);
 
     const addTodoHandler = () => {
         if (todo.length <= 0) {
             toast.error("Please write your task");
             return;
         }
-        dispatch(addToNewTodos([...newTodos,{
-            id: newTodos.length === 0 ? 0 : (newTodos[newTodos.length - 1].id) + 1,
+        dispatch(addToNewTodos([...newTodos, {
+            id: allTodos.length === 0 ? 0 : (allTodos[allTodos.length - 1].id) + 1,
             value: todo
         }]));
-    
-        dispatch(addToAllTdodos({
-            id: allTdodos.length === 0 ? 0 : (allTdodos[allTdodos.length - 1].id) + 1,
+
+        localStorage.setItem("newTodos", JSON.stringify([...newTodos, {
+            id: allTodos.length === 0 ? 0 : (allTodos[allTodos.length - 1].id) + 1,
+            value: todo
+        }]));
+
+        dispatch(addToAllTodos({
+            id: allTodos.length === 0 ? 0 : (allTodos[allTodos.length - 1].id) + 1,
             value: todo
         }));
+
+        localStorage.setItem("allTodos", JSON.stringify([...allTodos, {
+            id: allTodos.length === 0 ? 0 : (allTodos[allTodos.length - 1].id) + 1,
+            value: todo
+        }]));
 
         setTodo("");
         toast.success("Task added successfully !");
     }
+    useEffect(() => {
+       let existingCompletedTodos = localStorage.getItem("allTodos");
+       if (completedTodos) {
+            dispatch(addToCompletedTodos(JSON.parse(existingCompletedTodos)));
+       }
+        let existingNewTodos = localStorage.getItem("newTodos");
+        if (existingNewTodos) {
+            dispatch(addToNewTodos(JSON.parse(existingNewTodos)));
+        }
+    
+      return () => {
+        
+      }
+    }, [])
+    
 
     const onDragEndHandler = (result) => {
-        const { destination, source, draggableId } = result;
-        console.log(destination, "destination");
-        console.log(source, "source");
-        // console.log(result, "result");
+        const { destination, source } = result;
+
+
         if (destination && source) {
 
-            if (source.droppableId === "newTodosList" && destination.droppableId === "completedTodosList") {
+            if (source.droppableId === "newTodosList" && destination.droppableId === "newTodosList") {
+                let sourceTodos = JSON.parse(JSON.stringify(newTodos));
+                let add = sourceTodos[source.index];
+                sourceTodos.splice(source.index, 1);
+                sourceTodos.splice(destination.index, 0, add);
 
+                dispatch(addToNewTodos(sourceTodos));
+
+                localStorage.setItem("newTodos", JSON.stringify(sourceTodos));
+            }
+            if (source.droppableId === "inProgressTodosList" && destination.droppableId === "inProgressTodosList") {
+                let sourceTodos = JSON.parse(JSON.stringify(inProgressTodos));
+                let add = sourceTodos[source.index];
+                sourceTodos.splice(source.index, 1);
+                sourceTodos.splice(destination.index, 0, add);
+
+                dispatch(addToInProgressTodos(sourceTodos));
+
+                localStorage.setItem("inProgressTodos", JSON.stringify(sourceTodos));
+            }
+            if (source.droppableId === "completedTodosList" && destination.droppableId === "completedTodosList") {
+                let sourceTodos = JSON.parse(JSON.stringify(completedTodos));
+
+                let add = sourceTodos[source.index];
+                sourceTodos.splice(source.index, 1);
+                sourceTodos.splice(destination.index, 0, add);
+
+                dispatch(addToCompletedTodos(sourceTodos));
+
+                localStorage.setItem("completedTodos", JSON.stringify(sourceTodos));
+
+            }
+            if (source.droppableId === "newTodosList" && destination.droppableId === "completedTodosList") {
+                let sourceTodos = JSON.parse(JSON.stringify(newTodos));
+                let destinationTodos = JSON.parse(JSON.stringify(completedTodos));
+                let add = sourceTodos[source.index];
+                sourceTodos.splice(source.index, 1);
+                destinationTodos.splice(destination.index, 0, add);
+
+                dispatch(addToCompletedTodos(destinationTodos));
+                dispatch(addToNewTodos(sourceTodos));
+
+                localStorage.setItem("completedTodos", JSON.stringify(destinationTodos));
+                localStorage.setItem("newTodos", JSON.stringify(sourceTodos));
+            }
+            if (source.droppableId === "completedTodosList" && destination.droppableId === "newTodosList") {
+                let sourceTodos = JSON.parse(JSON.stringify(completedTodos));
+                let destinationTodos = JSON.parse(JSON.stringify(newTodos));
+                let add = sourceTodos[source.index];
+                sourceTodos.splice(source.index, 1);
+                destinationTodos.splice(destination.index, 0, add);
+
+                dispatch(addToCompletedTodos(sourceTodos));
+                dispatch(addToNewTodos(destinationTodos));
+
+                localStorage.setItem("completedTodos", JSON.stringify(sourceTodos));
+                localStorage.setItem("newTodos", JSON.stringify(destinationTodos));
             }
             if (source.droppableId === "newTodosList" && destination.droppableId === "inProgressTodosList") {
 
                 let sourceTodos = JSON.parse(JSON.stringify(newTodos));
-                console.log(sourceTodos, "sourceTodos");
                 let destinationTodos = JSON.parse(JSON.stringify(inProgressTodos));
                 let add = sourceTodos[source.index];
-                console.log(add, "add");
                 sourceTodos.splice(source.index, 1);
-
                 destinationTodos.splice(destination.index, 0, add);
 
                 dispatch(addToInProgressTodos(destinationTodos));
                 dispatch(addToNewTodos(sourceTodos));
-                
-    
+
+                localStorage.setItem("inProgressTodosList", JSON.stringify(destinationTodos));
+                localStorage.setItem("newTodos", JSON.stringify(sourceTodos));
             }
             if (source.droppableId === "inProgressTodosList" && destination.droppableId === "newTodosList") {
-                 
-                    let sourceTodos = JSON.parse(JSON.stringify(inProgressTodos));
-                    console.log(sourceTodos, "sourceTodos");
-                    let destinationTodos = JSON.parse(JSON.stringify(newTodos));
-                    let add = sourceTodos[source.index];
-                    console.log(add, "add");
-                    sourceTodos.splice(source.index, 1);
-    
-                    destinationTodos.splice(destination.index, 0, add);
-    
-                    dispatch(addToInProgressTodos(sourceTodos));
-                    dispatch(addToNewTodos(destinationTodos));
+                let sourceTodos = JSON.parse(JSON.stringify(inProgressTodos));
+                let destinationTodos = JSON.parse(JSON.stringify(newTodos));
+                let add = sourceTodos[source.index];
+                sourceTodos.splice(source.index, 1);
+                destinationTodos.splice(destination.index, 0, add);
+
+                dispatch(addToInProgressTodos(sourceTodos));
+                dispatch(addToNewTodos(destinationTodos));
+
+                localStorage.setItem("inProgressTodosList", JSON.stringify(sourceTodos));
+                localStorage.setItem("newTodos", JSON.stringify(destinationTodos));
             }
             if (source.droppableId === "inProgressTodosList" && destination.droppableId === "completedTodosList") {
                 let sourceTodos = JSON.parse(JSON.stringify(inProgressTodos));
-                console.log(sourceTodos, "sourceTodos");
                 let destinationTodos = JSON.parse(JSON.stringify(completedTodos));
                 let add = sourceTodos[source.index];
-                console.log(add, "add");
                 sourceTodos.splice(source.index, 1);
-
                 destinationTodos.splice(destination.index, 0, add);
 
                 dispatch(addToInProgressTodos(sourceTodos));
                 dispatch(addToCompletedTodos(destinationTodos));
+
+                localStorage.setItem("inProgressTodosList", JSON.stringify(sourceTodos));
+                localStorage.setItem("completedTodos", JSON.stringify(destinationTodos));
             }
             if (source.droppableId === "completedTodosList" && destination.droppableId === "inProgressTodosList") {
                 let sourceTodos = JSON.parse(JSON.stringify(completedTodos));
-                console.log(sourceTodos, "sourceTodos");
                 let destinationTodos = JSON.parse(JSON.stringify(inProgressTodos));
                 let add = sourceTodos[source.index];
-                console.log(add, "add");
                 sourceTodos.splice(source.index, 1);
-
                 destinationTodos.splice(destination.index, 0, add);
 
                 dispatch(addToInProgressTodos(destinationTodos));
                 dispatch(addToCompletedTodos(sourceTodos));
+
+                localStorage.setItem("inProgressTodosList", JSON.stringify(destinationTodos));
+                localStorage.setItem("completedTodos", JSON.stringify(sourceTodos));
             }
         }
 
@@ -128,7 +203,7 @@ const Todo = () => {
                                     <div className='p-2' ref={provided.innerRef} {...provided.droppableProps}>
                                         {
                                             newTodos.map((todo, index) => (
-                                                <Draggable draggableId={todo?.id.toString()+'newTodosList'} index={index} key={todo?.id+'newTodosListx'}>
+                                                <Draggable draggableId={todo?.id.toString() + 'newTodosList'} index={index} key={todo?.id + 'newTodosListx'}>
                                                     {(provided) => (
                                                         <div className='py-2'  {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                                                             <p className='secondary-bg text-dark text-center mb-0 p-1 border border-secondary'>{todo.value}</p>
@@ -153,7 +228,7 @@ const Todo = () => {
                                     <div className='p-2' ref={provided.innerRef} {...provided.droppableProps}>
                                         {
                                             inProgressTodos.map((todo, index) => (
-                                                <Draggable draggableId={todo?.id.toString()+'inProgressTodosList'} index={index} key={todo?.id+'inProgressTodosList'}>
+                                                <Draggable draggableId={todo?.id.toString() + 'inProgressTodosList'} index={index} key={todo?.id + 'inProgressTodosList'}>
                                                     {(provided) => (
                                                         <div className='py-2'  {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                                                             <p className='secondary-bg text-dark text-center mb-0 p-1 border border-secondary'>{todo.value}</p>
@@ -178,7 +253,7 @@ const Todo = () => {
                                     <div className='p-2' ref={provided.innerRef} {...provided.droppableProps}>
                                         {
                                             completedTodos.map((todo, index) => (
-                                                <Draggable draggableId={todo?.id.toString()+'completedTodosList'} index={index} key={todo?.id+'completedTodosList'}>
+                                                <Draggable draggableId={todo?.id.toString() + 'completedTodosList'} index={index} key={todo?.id + 'completedTodosList'}>
                                                     {(provided) => (
                                                         <div className='py-2'  {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
                                                             <p className='secondary-bg text-dark text-center mb-0 p-1 border border-secondary'>{todo.value}</p>
--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -3,12 +3,11 @@ import { createSlice } from '@reduxjs/toolkit'
 
 
 const initialState = {
-    allTdodos: [],
+    allTodos: [],
     newTodos: [],
     inProgressTodos: [],
     completedTodos: [],
 }
-
 
 
 
@@ -17,27 +16,19 @@ export const todosSlice = createSlice({
     initialState,
     reducers: {
     
-        addToAllTdodos: (state, action) => {
-            state.allTdodos = [...state.allTdodos, action.payload]
+        addToAllTodos: (state, action) => {
+            state.allTodos = [...state.allTodos, action.payload]
         },
         addToNewTodos: (state, action) => {
            state.newTodos = action.payload;
 
         },
-        removeToNewTodos: (state, action) => {
-             state.newTodos.splice(action.payload,1);
-            
-           
-        },
+      
         addToInProgressTodos: (state,action) => {
           state.inProgressTodos = action.payload;
 
         },
-        removeToInProgressTodos: (state, action) => {
-            
-            state.inProgressTodos = state.inProgressTodos.filter((todo) => todo.id !== action.payload);
-           
-        },
+       
         addToCompletedTodos: (state,action) => {
            state.completedTodos = action.payload;
         },
@@ -46,5 +37,5 @@ export const todosSlice = createSlice({
 
 })
 // Action creators are generated for each case reducer function
-export const { addToNewTodos, addToInProgressTodos, addToCompletedTodos, removeToNewTodos,addToAllTdodos,removeToInProgressTodos, } = todosSlice.actions
+export const { addToNewTodos, addToInProgressTodos, addToCompletedTodos,addToAllTodos, } = todosSlice.actions
 export default todosSlice.reducer
